95c6b6b900359acbb4609efc83f59f69a3be770f
leftcousoin <191420874@qq.com>
2022-03-20 15:19:50 +0800

    update

--- Springmvc01/src/main/java/over/controller/Controller01.java
+++ Springmvc01/src/main/java/over/controller/Controller01.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller01 {
+    //rest风格
+
     @RequestMapping("/test")
     public Object test01(){
         return "aaaa";
